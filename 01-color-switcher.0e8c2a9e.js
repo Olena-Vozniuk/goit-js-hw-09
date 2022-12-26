c3a08e341f393af14c5e122032d99c1bb63e17e2
Olena-Vozniuk <110108199+Olena-Vozniuk@users.noreply.github.com>
2022-12-26 16:28:28 +0000

    Deploying to gh-pages from @ Olena-Vozniuk/goit-js-hw-09@d6898e21e3b7948ba98e505e8bc44dde32eace5b 🚀

---
+++ 01-color-switcher.0e8c2a9e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body;t.addEventListener("click",(function(){intervalId=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(intervalId),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.0e8c2a9e.js.map
