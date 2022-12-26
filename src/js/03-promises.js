3038fd4aa7a6f2ab681c8039112552dbc463dec4
Olena-Vozniuk <110108199+Olena-Vozniuk@users.noreply.github.com>
2022-12-26 18:48:17 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve ({ position, delay })
+      } else {
+        reject ({ position, delay })
+      }
+    }, delay);
+  });
+};
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  
+  let delay = Number(evt.currentTarget.delay.value);
+  let step = Number(evt.currentTarget.step.value);
+  let amount = Number(evt.currentTarget.amount.value);
+
+  for (let position = 0; position < amount; position+=1) {
+  createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += step;
   }
-}
+  evt.currentTarget.reset();
+};
+
+
